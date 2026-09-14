f28698409cbc670468cddfbdd3899123ec324ffc
Riyadulislamratul <riadul1810833319@gmail.com>
2026-09-14 23:37:00 +0600

    Header added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const App = () => {
+import MainLayout from "./components/MainLayout";
+
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import Jobs from "./pages/Jobs";
+import Pricing from "./pages/Pricing";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+
+function App() {
   return (
-    <div>App</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route path="products" element={<Products />} />
+          <Route path="jobs" element={<Jobs />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+  const navItems = [
+    { name: "Home", path: "/" },
+    { name: "Products", path: "/products" },
+    { name: "Jobs", path: "/jobs" },
+    { name: "Pricing", path: "/pricing" },
+    { name: "About", path: "/about" },
+  ];
 
-export default Header
+  return (
+    <header className="relative z-50 mx-3 flex h-[58px] items-center justify-between bg-[#243945] px-6 md:px-10 lg:px-[108px]">
+      {/* Logo */}
+      <NavLink
+        to="/"
+        className="flex items-center gap-2 text-[15px] font-semibold tracking-wide text-white"
+      >
+        <div className="relative h-7 w-7">
+          <span className="absolute left-0 top-[5px] h-[19px] w-[12px] border-2 border-white" />
+          <span className="absolute left-[7px] top-0 h-[19px] w-[12px] border-2 border-white" />
+        </div>
+
+        <span>ISTAIX</span>
+      </NavLink>
+
+      {/* Desktop Navigation */}
+      <nav className="hidden items-center gap-8 md:flex">
+        {navItems.map((item) => (
+          <NavLink
+            key={item.path}
+            to={item.path}
+            className={({ isActive }) =>
+              `text-[14px] transition ${
+                isActive
+                  ? "font-medium text-white"
+                  : "text-white/90 hover:text-white"
+              }`
+            }
+          >
+            {item.name}
+          </NavLink>
+        ))}
+      </nav>
+
+      {/* CTA */}
+      <NavLink
+        to="/contact"
+        className="rounded-[4px] bg-[#8da8b8] px-6 py-2.5 text-[13px] font-medium text-white transition hover:bg-[#a1b8c6]"
+      >
+        Get started
+      </NavLink>
+    </header>
+  );
+};
+
+export default Header;
